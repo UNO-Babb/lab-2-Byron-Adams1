47800f5e92e2b1385838e231ebf7e928503c5ef9
Byron-Adams1 <byronadams2025@outlook.com>
2025-01-29 16:14:55 +0000

    Done

--- FutureTime.py
+++ FutureTime.py
@@ -15,27 +15,20 @@ def main():
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
-  #Ask user for hours
-  hours = input("Enter hours: ") 
-  hours =int(hours)  % 24 #changes text to numbers
-  futureHours = currentHour + hours
-  futureHours = int(futureHours) % 24
-
-
+  
   #Ask user for minutes
   minutes = input("Enter minutes: ")
   minutes = int(minutes)
   futureMinutes = currentMinute + minutes
   futureMinutes = futureMinutes % 60
-
-
-  #Calculate the time after the user-supplied time has passed.
-
-
-  #Do not use any if statements in calculating the time.  
- 
+  extraMinutes = minutes // 60 
   
-  
+  #Ask user for hours
+  hours = input("Enter hours: ") 
+  hours =int(hours)  % 24 #changes text to numbers
+  futureHours = currentHour + hours + extraMinutes
+  futureHours = int(futureHours) % 24
+
   #Output the future time in standard format "HH:MM"
   print (str(futureHours) + ":" + str(futureMinutes))
 
