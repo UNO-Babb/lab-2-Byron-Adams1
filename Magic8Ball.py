810b9561478c8fbfaa349335d51b2293b1cf72f6
Byron-Adams1 <byronadams2025@outlook.com>
2025-01-29 14:58:02 +0000

    8 ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,44 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Byron Adams
+#Date: 1/29/2025
+#Assignment: Magic 8 Ball
 
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
-  print("Magic 8 Ball")
+  print("-Magic 8 Ball-")
+answers = [
+  "As I see it, yes",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again.",
+  "Don’t count on it",
+  "It is certain",
+  "It is decidedly so",
+  "Most likely",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Outlook good",
+  "Reply hazy, try again",
+  "Signs point to yes",
+  "Very doubtful",
+  "Without a doubt",
+  "Yes",
+  "Definitely",
+  "You may rely on it."
+  ]
+
   #Prompt the user for their question.
 
+question = input("Please ask me a general or very specific yes or no question.")
   #Answer question randomly with one of the options from your earlier list.
 
+response = random.choice(answers)
+print (response)
 
 if __name__ == '__main__':
   main()
